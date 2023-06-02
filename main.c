30d9c588251968c639446472ccd578e48913621a
marcelo914 <marcelosouza222244@gmail.com>
2023-06-02 14:45:09 -0300

    add

--- main.c
+++ main.c
@@ -1,24 +1,31 @@
 #include "cadastro.h"
-#include "password.h"
 #include <complex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
 void cadastro(int s[60]){
-  char nome[60];
-  int login, password, rptpassword;
+  char nome[60], psize[100];
+  int login, password, rptpassword, intpsize, try;
   float renda;
+  try = 3;
 
   system("clear||cls");
+
   printf("Bem vindo\n");
   printf("\"nome do banco\"\n\n");
+
+
   printf("Escreva o seu nome completo a seguir: \n");
   login = user_name(nome);
   printf("Digite a sua renda mensal: \n");
   renda = income(renda);
   printf("Define a sua senha:\n");
-  password = senha(password);
 
 }
+
+
 int main() {
   int v[60];
   cadastro(v);
--- password.h
+++
@@ -1,8 +0,0 @@
-#ifndef PASSWORD_H
-#define PASSWORD_H
-#include <stdio.h>
-int senha(int password){
-  scanf("%d", &password);
-}
-
-#endif // !DEBUG
