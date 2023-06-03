7e8774b13f5d9d354247ad274c94caae4fe189e0
marcelo914 <marcelosouza222244@gmail.com>
2023-06-03 11:16:45 -0300

    add

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include "cadastro.h"
 #include <complex.h>
-#include "start.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+int loginsuc;
 
 void cadastro(int s[60]){
   char nome[60], strpassword[60];
   int login, password, confpassword, sizepassw, try, choose, n, m;
+  long int numgerente;
   double renda, saldo, menu_saldo;
   int extrato[n][m];
   try = 3;
-
+  numgerente =  61998007307;
 
 
   system("clear||cls");
@@ -48,33 +48,58 @@ void cadastro(int s[60]){
       scanf("%d", &confpassword);
       try --;
     }
+    if (try <= 0) {
+      printf("entre em contato com o gerente %ld\n", numgerente);
+    }
   }
   if(confpassword == password) {
     printf("cadastro pronto\n");
+    loginsuc = 1;
     
   }
 }
 void menu_opcoes(int s[60]){
-  int choose;
-  printf("Bem vindo ao menu de opções!\n\n");
-  printf("veja as opções a seguir: \n");
-  printf("Saldo da conta (1)\n");
-  printf("Deposito (2)\n");
-  printf("Sacar (3)\n");
-  printf("trasferência(4)\n");
-  printf("configurações(5)\n");
+  int choices;
 
-  scanf("%d", &choose);
-   switch (choose) {
-    case 1:
+    printf("\n\n -----------------------");
+    printf("Escolha uma opção:\n");
+    printf("\n");
+    printf("1 - Saldo\n");
+    printf("2 - Deposito\n");
+    printf("3 - Sacar\n");
+    printf("4 - Transferencia\n");
+    printf("5 - Outros\n");
+    printf("\n");
+    printf("Inserir outra opção:\n");
+    scanf("%d", &choices);
+  
+  switch (choices) {
+    case 1 :
+    printf ("1 - Saldo.\n");
+    break;
+    
+    case 2 :
+    printf ("2 - Depósito.\n");
+    break;
+    
+    case 3 :
+    printf ("3 - Sacar.\n");
+    break;
+    
+    case 4 :
+    printf ("4 - Transferência.\n");
+    break;
+    
+    case 5 :
+    printf ("5 - Outros.\n");
+    break;
+    }
   }
-}
-
-
-
 int main() {
   int v[60];
   cadastro(v);
-  menu_opcoes(v);
+  if (loginsuc == 1) {
+    menu_opcoes(v);
+  }
   return 0;
 }
